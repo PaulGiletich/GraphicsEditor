75e0d3c75de16900629c6c666136e9fa03efc657
liroki <liroki.666@gmail.com>
2013-12-16 06:05:50 +0200

    About change

--- src/com/pgiletich/graphics/ui/AboutWindow.java
+++ src/com/pgiletich/graphics/ui/AboutWindow.java
@@ -1,14 +1,12 @@
 package com.pgiletich.graphics.ui;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class AboutWindow extends JFrame {
 
     public AboutWindow(){
-        setLayout(new FlowLayout());
-        setSize(250, 100);
-        add(new JLabel("Authors: Paul Giletich, Alexander Vasilenko"));
-        add(new JLabel("group 021701"));
+        setSize(300, 100);
+        this.add(new JLabel("Author: Paul Giletich"));
+        this.add(new JLabel("group 021701"));
     }
 }
